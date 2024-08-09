11f966ab9178d24dfaebc13253da4aa866efe7b5
Duvan Zavaleta <duvzavaleta@gmail.com>
2024-08-09 18:44:12 -0500

    Update idca_grp

--- idca_grp/models/account_analytic_account.py
+++ idca_grp/models/account_analytic_account.py
@@ -7,8 +7,36 @@ _logger = logging.getLogger(__name__)
 class AccountAnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
 
+    importe_comprometido = fields.Float(string="Importe comprometido", compute="_compute_importe_comprometido")
+    importe_devengado = fields.Float(string="Importe devengado", compute="_compute_importe_devengado")
     available_budget = fields.Float(string="Presupuesto disponible", compute="_compute_available_budget")
 
     def _compute_available_budget(self):
         for line in self:
             line.available_budget = line.planned_amount + line.practical_amount
+
+
+    #Check for importe_comprometido
+    def _compute_importe_comprometido(self):
+        for line in self:
+            if line.analytic_account_id:
+                purchase_lines = self.env['purchase.order.line'].search([
+                    ('order_id.state', '=', 'draft'),
+                    ('analytic_distribution', 'ilike', line.analytic_account_id.id)
+                ])
+                line.importe_comprometido = sum(purchase_lines.mapped('price_total'))
+            else:
+                line.importe_comprometido = 0.0
+
+
+    #Check for importe_devengado
+    def _compute_importe_devengado(self):
+        for line in self:
+            if line.analytic_account_id:
+                purchase_lines = self.env['purchase.order.line'].search([
+                    ('order_id.state', '=', 'purchase'),
+                    ('analytic_distribution', 'ilike', line.analytic_account_id.id)
+                ])
+                line.importe_devengado = sum(purchase_lines.mapped('price_total'))
+            else:
+                line.importe_devengado = 0.0
--- idca_grp/models/crossovered_budget_lines.py
+++ idca_grp/models/crossovered_budget_lines.py
@@ -12,9 +12,49 @@ class CrossoveredBudgetLines(models.Model):
     funcion_id = fields.Many2one('idca.funcion', string="Función")
     subfuncion_id = fields.Many2one('idca.subfuncion', string="Subfunción")
 
+    budget_account_analytic = fields.Char(string="Budget Account Analytic", compute="_compute_budget_account_analytic")
+
+    def _compute_budget_account_analytic(self):
+        for record in self:
+            if record.budget_details_id and record.analytic_account_id:
+                record.budget_account_analytic = f"{record.budget_details_id.name} - {record.analytic_account_id.name}"
+            else:
+                record.budget_account_analytic = ''
+
+
+    importe_comprometido = fields.Float(string="Importe comprometido", compute="_compute_importe_comprometido")
+    importe_devengado = fields.Float(string="Importe devengado", compute="_compute_importe_devengado")
     available_budget = fields.Float(string="Presupuesto disponible", compute="_compute_available_budget")
 
     def _compute_available_budget(self):
         for line in self:
             line.available_budget = line.planned_amount + line.practical_amount
+
+
+
+
+    #Check for importe_comprometido
+    def _compute_importe_comprometido(self):
+        for line in self:
+            if line.analytic_account_id:
+                purchase_lines = self.env['purchase.order.line'].search([
+                    ('order_id.state', '=', 'draft'),
+                    ('analytic_distribution', 'ilike', line.analytic_account_id.id)
+                ])
+                line.importe_comprometido = sum(purchase_lines.mapped('price_total'))
+            else:
+                line.importe_comprometido = 0.0
+
+
+    #Check for importe_devengado
+    def _compute_importe_devengado(self):
+        for line in self:
+            if line.analytic_account_id:
+                purchase_lines = self.env['purchase.order.line'].search([
+                    ('order_id.state', '=', 'purchase'),
+                    ('analytic_distribution', 'ilike', line.analytic_account_id.id)
+                ])
+                line.importe_devengado = sum(purchase_lines.mapped('price_total'))
+            else:
+                line.importe_devengado = 0.0
 
